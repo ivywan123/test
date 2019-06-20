92818c6da94f59d2f84cb2a4bbf1cf3cf87a26ab
wan <363701080@qq.com>
2019-06-20 15:55:59 +0800

    123

---
+++ src/main/java/com/niu/cntr/Service/TradeDaoImpl/T_cntrServiceImpl.java
@@ -0,0 +1,17 @@
+package com.niu.cntr.Service.TradeDaoImpl;
+
+import com.niu.cntr.Service.TradeService.T_cntrService;
+import com.niu.cntr.mapper.t_cntrmapper;
+import com.niu.cntr.mybatisConfig.DataSourceSqlSessionFactory;
+import com.niu.cntr.mybatisConfig.MapperFactory;
+
+import java.math.BigDecimal;
+
+public class T_cntrServiceImpl implements T_cntrService {
+    @Override
+    public Integer updateProfit(BigDecimal profit, Long cntr_id) {
+        t_cntrmapper mapper = MapperFactory.createMapper(t_cntrmapper.class, DataSourceSqlSessionFactory.DataSourceEnvironment.trade);
+        Integer result  = mapper.updateProfit(profit,cntr_id);
+        return result;
+    }
+}
